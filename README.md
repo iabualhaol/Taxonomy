41fcfe3fac4e53f4c5ded1f5186bb94ac127ca57
Michael Weiss <mrw@acm.org>
2015-11-11 10:22:46 -0500

    set access control allow origin policy

---
+++ README.md
@@ -0,0 +1,10 @@
+# Running the code
+
+1. Go into the server directory:
+> cd server
+
+2. Start the web server:
+> http.sh
+
+3. Start the application server:
+> go run *.go
--- server/handlers.go
+++ server/handlers.go
@@ -11,6 +11,7 @@ func IndexHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func NodeIndexHandler(w http.ResponseWriter, r *http.Request) {
+	SetHeaders(w)
 	if err := json.NewEncoder(w).Encode(GetAllNodes()); err != nil {
 		panic(err)
 	}
@@ -19,6 +20,7 @@ func NodeIndexHandler(w http.ResponseWriter, r *http.Request) {
 func NodeViewHandler(w http.ResponseWriter, r *http.Request) {
   vars := mux.Vars(r)
 	nodeId := vars["id"]
+	SetHeaders(w)
 	if err := json.NewEncoder(w).Encode(GetNode(nodeId)); err != nil {
 		panic(err)
 	}
@@ -27,12 +29,14 @@ func NodeViewHandler(w http.ResponseWriter, r *http.Request) {
 func CreateNodeHandler(w http.ResponseWriter, r *http.Request) {
 	label := r.FormValue("label")
 	node := AddNode(Node { Label: label })
+	SetHeaders(w)
 	if err := json.NewEncoder(w).Encode(node); err != nil {
 		panic(err)
 	}
 }
 
 func EdgeIndexHandler(w http.ResponseWriter, r *http.Request) {
+	SetHeaders(w)
 	if err := json.NewEncoder(w).Encode(GetAllEdges()); err != nil {
 		panic(err)
 	}
@@ -41,6 +45,7 @@ func EdgeIndexHandler(w http.ResponseWriter, r *http.Request) {
 func EdgeViewHandler(w http.ResponseWriter, r *http.Request) {
   vars := mux.Vars(r)
 	id := vars["id"]
+	SetHeaders(w)
 	fmt.Fprintln(w, "Edge:", id)
 }
 
@@ -48,6 +53,7 @@ func CreateEdgeHandler(w http.ResponseWriter, r *http.Request) {
 	from := r.FormValue("from")
 	to := r.FormValue("to")
 	edge := AddEdge(Edge { From: from, To: to })
+	SetHeaders(w)
 	if err := json.NewEncoder(w).Encode(edge); err != nil {
 		panic(err)
 	}
@@ -58,6 +64,7 @@ func NodeEvidenceIndexHandler(w http.ResponseWriter, r *http.Request) {
 	nodeId := vars["id"]
 	items := GetEvidence(nodeId)
 	fmt.Println("Evidence for node: ", nodeId, " is: ", items)
+	SetHeaders(w)
 	if err := json.NewEncoder(w).Encode(items); err != nil {
 		panic(err)
 	}
@@ -70,7 +77,14 @@ func NodeCreateEvidenceHandler(w http.ResponseWriter, r *http.Request) {
 	AddEvidence(EvidenceItem { NodeId: nodeId, Author: "Michael", Vote: vote, 
 		Reason: r.FormValue("reason") })
 	items := GetEvidence(nodeId)
+	SetHeaders(w)
 	if err := json.NewEncoder(w).Encode(items); err != nil {
 		panic(err)
 	}
+}
+
+func SetHeaders(w http.ResponseWriter) {
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+	w.WriteHeader(http.StatusOK)
 }
