c05f20ef64b1f16e2579807a6cb7d31ebfa5dc57
Michael Weiss <mrw@acm.org>
2015-11-13 00:33:53 -0500

    show average evidence

--- client/http.sh
+++ client/http.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 ip=`ipconfig getifaddr en0`
-cat addNodesBootcards.html | sed "s/localhost/$ip/" > index.html
+cat visualizingEvidence.html | sed "s/localhost/$ip/" > index.html
 
 python -m SimpleHTTPServer 8000
