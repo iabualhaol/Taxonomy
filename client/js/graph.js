ed283f10a0976e652bf8b4744bbdbb8236bf43f9
Michael Weiss <mrw@acm.org>
2015-11-25 11:23:21 -0500

    added code to connect to server and load graph

--- client/js/graph.js
+++ client/js/graph.js
@@ -1,23 +1,36 @@
 var GraphViewModel = function() {
 	var self = this;
 
+	self.graph = {};
 	self.nodes = new vis.DataSet([]);
     self.edges = new vis.DataSet([]);
 
-    self.init = function() {
-     	return self;
-    }
-
-    self.createNetwork = function(container, options) {
+    self.createGraph = function(container, options) {
+    	var data = {
+			nodes: self.nodes,
+			edges: self.edges
+	  	};
+	  	self.graph = new vis.Network(container, data, options);
     	self.loadNodes();
     	self.loadEdges();
     }
 
     self.loadNodes = function() {
     	console.log('loadNodes');
+    	$.getJSON("http://localhost:8080/nodes", function(data) {
+		    self.nodes.update(data); 
+		    if (self.nodes.length > 0) {
+		    	// self.firstNode(false);
+		    }
+		    self.graph.fit(); 
+		});
     }
 
     self.loadEdges = function() {
     	console.log('loadEdges');
+    	$.getJSON("http://localhost:8080/edges", function(data) {
+		    self.edges.update(data);  
+		    self.graph.fit();
+		});
     }
 }
