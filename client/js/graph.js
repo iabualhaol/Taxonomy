15d76aa00751c5748481e97607b60e27608322b7
Michael Weiss <mrw@acm.org>
2015-11-25 10:59:53 -0500

    add logic to add network

--- client/js/graph.js
+++ client/js/graph.js
@@ -1,3 +1,23 @@
 var GraphViewModel = function() {
 	var self = this;
+
+	self.nodes = new vis.DataSet([]);
+    self.edges = new vis.DataSet([]);
+
+    self.init = function() {
+     	return self;
+    }
+
+    self.createNetwork = function(container, options) {
+    	self.loadNodes();
+    	self.loadEdges();
+    }
+
+    self.loadNodes = function() {
+    	console.log('loadNodes');
+    }
+
+    self.loadEdges = function() {
+    	console.log('loadEdges');
+    }
 }
